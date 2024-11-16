d0e9e080d42e490fcd61325cdb01735f0afd131f
omar0azx <a3mr.azx@gmail.com>
2024-11-17 00:45:34 +0300

    Add: mapVC, Add: opportunityMakerVC

---
+++ Tatawei Organisation/MVC/Controller/Cells/IconCell.swift
@@ -0,0 +1,12 @@
+//
+//  IconCell.swift
+//  Tatawei Organisation
+//
+//  Created by omar alzhrani on 14/05/1446 AH.
+//
+
+import UIKit
+
+class IconCell: UICollectionViewCell {
+    
+}
---
+++ Tatawei Organisation/MVC/Controller/OpportunityMakerVC.swift
@@ -0,0 +1,29 @@
+//
+//  OpportunityMakerVC.swift
+//  Tatawei Organisation
+//
+//  Created by omar alzhrani on 14/05/1446 AH.
+//
+
+import UIKit
+
+class OpportunityMakerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
